0a7d31cdd8e46c586bd77828fb30e5b5c64c8a5c
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-07 10:46:48 +0200

    created user model form

--- kickbox_academy/core/models.py
+++ kickbox_academy/core/models.py
@@ -1,16 +1,46 @@
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
+# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import User, Group, Permission
 from django.db import models
 
 
-# Create your models here.
-class UserProfile(models.Model):
-    MAX_NAME_LENGTH = 30
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=MAX_NAME_LENGTH)
-    last_name = models.CharField(max_length=MAX_NAME_LENGTH)
-    image = models.ImageField(upload_to='images/')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    groups = models.ManyToManyField(Group, verbose_name=('groups'), blank=True, related_name='customer_set')
+    user_permissions = models.ManyToManyField(Permission, verbose_name=('user permissions'), blank=True,
+                                              related_name='customuser_set')
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return f'{self.first_name}, {self.last_name}'
+        return self.email
--- kickbox_academy/core/views.py
+++ kickbox_academy/core/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 
-from kickbox_academy.core.forms import CreateUserForm
-
 
 # Create your views here.
 
@@ -11,15 +9,6 @@ def home_page(request):
 
 
 def join_us_page(request):
-    form = CreateUserForm
-    context = {'form': form}
-
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login-page')
-
     return render(request=request, template_name='join_us.html', context=context)
 
 
