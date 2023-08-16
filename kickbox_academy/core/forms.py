af2d6fe54a17a0a3f1599724d0225d1a74e61670
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-08-16 10:47:55 +0200

    fixed login active links in navbar

--- kickbox_academy/core/forms.py
+++ kickbox_academy/core/forms.py
@@ -1,23 +1,18 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from kickbox_academy.core.models import CustomUser
 
 
-class UserProfileForm(forms.ModelForm):
-    password_confirmation = forms.CharField(widget=forms.PasswordInput)
+class UserProfileForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+        fields = ['username', 'email', 'first_name', 'last_name']
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        password_confirmation = cleaned_data.get('password_confirmation')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        if password and password_confirmation and password != password_confirmation:
-            raise forms.ValidationError("Passwords do not match.")
+
 
 
 class UserLoginForm(AuthenticationForm):
--- kickbox_academy/core/views.py
+++ kickbox_academy/core/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
